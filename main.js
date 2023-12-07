317a2b6525fbb8fccacb45c4121b0e46a4685859
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-12-07 17:45:56 -0300

    07122302

--- main.js
+++ main.js
@@ -2,12 +2,18 @@
 
 console.log("Bem-vindo ao nosso site institucional!");
 
+
 console.log("Banner Rotativo")
+
 
 console.log("Seção 02")
 
+
 console.log("Sobre Nós")
+
 
 console.log("Section 03")
 
+
 console.log("Section 04")
+
